196a0d01f291d055042a5df78a35e2f4902afbcd
Trisha Kholiya <tktrishakholiya@gmail.com>
2025-12-03 07:32:02 -0800

    Created a custom exception for Matrix class and abstracted into its own file

--- src/custom_exception.cpp
+++ src/custom_exception.cpp
@@ -3,5 +3,6 @@
 
 class InvalidMatrixSize : public std::domain_error {
 public:
-	InvalidMatrixSize(const std::string& what_arg) : std::domain_error(what_arg) {}
+	InvalidMatrixSize(const std::string& what_arg) : std::domain_error(what_arg) 
+    {}
 };

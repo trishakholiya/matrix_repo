c82b3fa8279ed824f1981598b4769f8e14e9aa95
Trisha Kholiya <tktrishakholiya@gmail.com>
2025-12-04 22:22:11 -0800

    First attempt implementation of QL function

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include "custom_exception.cpp"
+#include "helper_func.cpp" // numerical recipes helper functions
 
 typedef std::vector<double> vec;
 typedef std::vector<vec> mat;
@@ -46,13 +47,11 @@ public:
   }
 
   TridiagonalResult householder_tridiagonalize();
-  QREigenResult qr_eigen_tridiagonal();
-
+  QREigenResult QL(std::vector<double> d, std::vector<double> e);
 
   // equal to a matrix operator
   // overload print << operator
 
-  
   // overload addition operator
   inline Matrix operator+(const Matrix& other) const {
     // check if matrices are able to be added
@@ -100,7 +99,7 @@ public:
   }
 };
 
-struct TridiagonalResult {
+struct TridiagonalResult { 
     Matrix T; // tridiagonal matrix
     Matrix Q_house; // accumulated Householder transforms
 };
@@ -116,10 +115,86 @@ inline TridiagonalResult Matrix::householder_tridiagonalize() {
     return result;
 }
 
-inline QREigenResult Matrix::qr_eigen_tridiagonal() {
-    QREigenResult result;
-    // trisha implement
-    return result;
+inline QREigenResult Matrix::QL(std::vector<double> d, std::vector<double> e) {
+  QREigenResult result;
+  int n = d.size();
+  int m, l, iter, i, k;
+  double s, r, p, g, f, dd, c, b;
+  const double eps=std::numeric_limits<double>::epsilon();
+
+
+  // initialize Z matrix (to store eigenvectors)
+  std::vector<std::vector<double>> z(n, std::vector<double>(n, 0.0));
+  for (i = 0; i < n; i++) {
+    z[i][i] = 1.0;
+  }
+
+  for (i = 1; i < n; i++) {
+    e[i-1] = e[i];
+  }
+  e[n-1] = 0.0;
+
+  for ( l=0; l<n; l++ ) {
+    iter = 0;
+    do {
+      for ( m=l; m<n-1; m++ ) {
+        dd = std::abs(d[m]) + std::abs(d[m+1]);
+        if ( std::abs(e[m]) <= eps*dd ) break;
+      }
+      if ( m!=l ) {
+        if ( iter++ == 30 ) throw std::runtime_error("Too many iterations in tqli");
+        g = (d[l+1] - d[l]) / (2.0 * e[l]);
+        r = pythag(g, 1.0);
+        g = d[m] - d[l] + e[l]/(g + SIGN(r,g));
+        s = 1.0;
+        c = 1.0;
+        p = 0.0;
+
+        for (i = m-1; i >= l; i--) {
+          f = s * e[i];
+          b = c * e[i];
+
+          e[i+1] = (r=pythag(f, g));
+          if ( r == 0 ) {
+            d[i+1] -= p;
+            e[m] = 0.0;
+            break;
+          }
+          s = f / r;
+          c = g / r;
+          g = d[i+1]-p;
+          r =  (d[i] - g)*s+2.0*c*b;
+          d[i+1]  = g + (p=s*r);
+          g = c*r - b;
+          
+          // FORM EIGENVECTORS
+          for ( k=0; k<n; k++ ) {
+            f = z[k][i+1];
+            z[k][i+1] = s*z[k][i]+c*f;
+            z[k][i] = c*z[k][i]-s*f;
+          }
+
+        }
+        if ( r== 0.0 && i >= l) continue;
+        d[l] -= p;
+        e[l] = g;
+        e[m] = 0.0;
+      }
+    } while ( m != l );
+  }
+
+  // flatten z for now to construct matrix
+  vec flatZ;
+  flatZ.reserve(n * n);
+
+  for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+          flatZ.push_back(z[i][j]);
+      }
+  }
+  result.eigenvalues = d;
+  result.Q_qr = Matrix(flatZ, n, n);
+  return result;
 }
 
   /*

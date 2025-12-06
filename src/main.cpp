d6565b393b296ce536314ab7e93707e5e2a2e4cf
Juliet James <juliet_james@berkeley.edu>
2025-12-05 19:55:31 -0800

    updated include paths

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-//#include <armadillo>
-//#include "benchmark.cpp"
+#include <armadillo>
+#include "benchmark.hpp"
 #include "Matrix.hpp"
 #include <vector>
-
-void print_vec(const std::vector<double>& v, const std::string& name) {
-    std::cout << name << ": ";
-    for (double x : v) std::cout << x << " ";
-    std::cout << "\n";
-}
 
 int main(int argc, char* argv[]) {
 
@@ -27,13 +21,11 @@ int main(int argc, char* argv[]) {
   Matrix mat4 = mat2 + mat3;
   mat4.print();
 
-  /*
   // benchmark the time
   auto matrix_sum_func = [&]() {
     Matrix temp = mat2 + mat3;
   };
 
-  
   // initialize matrixes with same values in armadillo
   arma::mat A = { {1, 2},
                   {3, 4} };
@@ -50,35 +42,6 @@ int main(int argc, char* argv[]) {
 
   std::cout << "Matrix Sum Time: " << matrix_time << '\n';
   std::cout << "Armadillo Sum Time: " << arma_time << '\n';
-  */
-
-  // test tridiagonalize function
-  TridiagonalResult tri1 = mat1.householder_tridiagonalize(true);
-
-  print_vec(tri1.d, "d1");
-  print_vec(tri1.e, "e1");
-
-  std::cout << "Q_house1:\n";
-  tri1.Q_house.print();
-
-
-  TridiagonalResult tri2 = mat2.householder_tridiagonalize(true);
-
-  print_vec(tri2.d, "d2");
-  print_vec(tri2.e, "e2");
-
-  std::cout << "Q_house2:\n";
-  tri2.Q_house.print();
-
-
-  TridiagonalResult tri3 = mat3.householder_tridiagonalize(true);
-
-  print_vec(tri3.d, "d3");
-  print_vec(tri3.e, "e3");
-
-  std::cout << "Q_house3:\n";
-  tri3.Q_house.print();
-
 
   return 0;
 }

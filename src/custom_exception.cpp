2add5aa9973b6774884d1b8b06281c17dae93713
Juliet James <juliet_james@berkeley.edu>
2025-12-05 19:44:00 -0800

    removing .cpp files that should be .hpp files

--- src/custom_exception.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdexcept>
-
-class InvalidMatrixSize : public std::domain_error {
-public:
-	InvalidMatrixSize(const std::string& what_arg) : std::domain_error(what_arg) 
-    {}
-};
